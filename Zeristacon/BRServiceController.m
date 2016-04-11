24d535c53eaaa93c693e56b0c6411c12752e34c5
Mountainbrussells <mountainbrussells@gmail.com>
2016-04-11 10:11:34 -0600

    Add ServiceController

---
+++ Zeristacon/BRServiceController.m
@@ -0,0 +1,13 @@
+//
+//  BRServiceController.m
+//  Zeristacon
+//
+//  Created by Ben Russell on 4/11/16.
+//  Copyright © 2016 Ben Russell. All rights reserved.
+//
+
+#import "BRServiceController.h"
+
+@implementation BRServiceController
+
+@end
