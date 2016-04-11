22396a64d8a34f5fe11743a3d27d0dccef70ee5d
Mountainbrussells <mountainbrussells@gmail.com>
2016-04-11 09:04:56 -0600

    Connect UITableView to ViewController

--- Zeristacon/Zeristacon/ViewController.m
+++ Zeristacon/Zeristacon/ViewController.m
@@ -8,7 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *eventTableView;
 
 @end
 
